7e094f8fcaaf181874060ccc76916ea91d4b2d51
takenakayuki <kbc18a06@stu.kawahara.ac.jp>
2020-09-02 14:05:53 +0900

    初期

---
+++ test_example2-2-3.py
@@ -0,0 +1,15 @@
+from calc import Calculator
+import pytest
+@pytest.fixture(scope="module")
+def calc():
+    c = Calculator()
+    print("create calculator")
+    return c
+def test_add(calc):
+    assert calc.add(5, 2) == 7
+def test_sub(calc):
+    assert calc.sub(5, 2) == 3
+def test_mul(calc):
+    assert calc.mul(5, 2) == 10
+def test_div(calc):
+    assert calc.div(5, 2) == 2
